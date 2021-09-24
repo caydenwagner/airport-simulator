380b085ccf5345f271de987827c9381ad19dfcd0
cayden wagner <wagn2541@pacificu.edu>
2021-09-24 10:22:46 -0700

    Implemented processErrors and lstCreate functions

--- ListADT/src/list.c
+++ ListADT/src/list.c
@@ -7,7 +7,6 @@
  Purpose:
  *************************************************************************/
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +14,7 @@
 #include "../include/list.h"
 
 char gszListErrors[NUMBER_OF_LIST_ERRORS][MAX_ERROR_LIST_CHARS];
+
 
 /**************************************************************************
  Function: 	 	lstLoadErrorMessages
@@ -34,9 +34,12 @@ void lstLoadErrorMessages ()
 
 /**************************************************************************
  Function: 	 	processError
+
  Description:   Process the error code passed to this routine
+
  Parameters:	pszFunctionName - function causing the error
  	 	 	 	errorCode 	    - identifies the list error
+
  Returned:	 	None
  *************************************************************************/
 
@@ -45,4 +48,45 @@ static void processError (const char *pszFunctionName, int errorCode)
 	printf ("Function: %s %s \n ", pszFunctionName,
 			 gszListErrors [errorCode]);
 	exit (EXIT_FAILURE);
+}
+
+/**************************************************************************
+ Function: 	 	lstCreate
+
+ Description:	Initializes pointers in the list to NULL and number of
+ 	 	 	 	elements to 0
+
+ Parameters:	psList - List pointer that is initialized to default
+ 	 	 	 	 	 	 values
+
+ Returned:		None
+ *************************************************************************/
+
+void lstCreate (ListPtr psList)
+{
+	if ( NULL == psList )
+	{
+		processError("lstCreate", ERROR_NO_LIST_CREATE);
+	}
+
+	psList->numElements = 0;
+	psList->psFirst = NULL;
+	psList->psCurrent = NULL;
+	psList->psLast = NULL;
+}
+
+/**************************************************************************
+ Function: 	 	lstInsertAfter
+
+ Description:
+
+ Parameters:	psList - List pointer that is initialized to default
+ 	 	 	 	 	 	 values
+
+ Returned:		None
+ *************************************************************************/
+
+void lstInsertAfter (ListPtr psList, const void *pBuffer, int size)
+{
+
 }
--- ListADT/src/listdriver.c
+++ ListADT/src/listdriver.c
@@ -12,6 +12,50 @@
 
 #include "../include/list.h"
 
+/****************************************************************************
+ Function: 	 	success
+
+ Description: 	print a success message
+
+ Parameters:	pszStr - the message to print
+
+ Returned:	 	none
+ ****************************************************************************/
+static void success(char *pszStr) {
+	printf("SUCCESS: %s\n", pszStr);
+}
+
+/****************************************************************************
+ Function: 	 	failure
+
+ Description:	print a failure message
+
+ Parameters:	pszStr - the message to print
+
+ Returned:	 	none
+ ****************************************************************************/
+static void failure(char *pszStr) {
+	printf("FAILURE: %s\n", pszStr);
+}
+
+/****************************************************************************
+ Function: 	 	assert
+
+ Description: 	if the expression is true, assert success; otherwise, assert
+ 	 	 	 	failure
+
+ Parameters:	szStr - the message to print
+
+ Returned:	 	none
+ ****************************************************************************/
+static void assert(bool bExpression, char *pTrue, char *pFalse) {
+	if (bExpression) {
+		success(pTrue);
+	} else {
+		failure(pFalse);
+	}
+}
+
 /**************************************************************************
  Function: 	 	main
  Description: 	test all the functionality of the list
@@ -20,7 +64,13 @@
  *************************************************************************/
 
 int main() {
+  lstLoadErrorMessages();
+  success("Loaded Error Messages");
+
+
+
   printf("Successful");
 
   return EXIT_SUCCESS;
 }
+
