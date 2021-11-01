a3eced63bca257049a2f2ed99174d69b2317d6c8
Cayden Wagner <wagn2541@pacificu.edu>
2021-10-31 17:06:35 -0700

    Added more airport structure, makefile is giving an error: redefinition of all PQ functions

--- Airport/include/airport.h
+++ Airport/include/airport.h
@@ -15,4 +15,47 @@
 #include "../../PriorityQueueADT/include/pqueue.h"
 #include "../../QueueADT/include/queue.h"
 
+//*************************************************************************
+// Constants
+//*************************************************************************
+#define MAX_ERROR_AIRPORT_CHARS 64
+
+enum {NO_AIRPORT_ERROR = 0,
+			ERROR_NO_AIRPORT_CREATE,
+			ERROR_INVALID_AIRPORT,
+			ERROR_EMPTY_AIRPORT}; // If this error name changes, change stmt below
+#define NUMBER_OF_AIRPORT_ERRORS ERROR_EMPTY_AIRPORT - NO_AIRPORT_ERROR + 1
+
+//*************************************************************************
+// Error Messages
+//*************************************************************************
+#define LOAD_AIRPORT_ERRORS strcpy(gszAirportErrors[NO_AIRPORT_ERROR], "No Error.");\
+strcpy(gszAirportErrors[ERROR_NO_AIRPORT_CREATE], "Error: No AIRPORT Create.");\
+strcpy(gszAirportErrors[ERROR_INVALID_AIRPORT], "Error: Invalid AIRPORT.");\
+strcpy(gszAirportErrors[ERROR_EMPTY_AIRPORT], "Error: Empty AIRPORT.");
+
+//*************************************************************************
+// User-defined types
+//*************************************************************************
+
+typedef struct Airplane
+{
+	int fuel;
+} Airplane;
+typedef Airplane* AirplanePtr;
+
+
+typedef struct Airport *AirportPtr;
+typedef struct Airport
+{
+	Queue sRunwayQueue;
+	PriorityQueue sInFlightPQueue;
+	char aRunwayStatus[3];
+	int timer;
+} Airport;
+
+extern void airportCreate (AirportPtr);
+
+extern void airportLoadErrorMessages ();
+
 #endif /* AIRPORT_H_ */
--- Airport/src/airport.c
+++ Airport/src/airport.c
@@ -10,4 +10,34 @@
 #include <string.h>
 #include <stdbool.h>
 #include "../include/airport.h"
+#include "../../PriorityQueueADT/src/pqueue.c"
+#include "../../QueueADT/src/queue.c"
 
+char gszAirportErrors[NUMBER_OF_AIRPORT_ERRORS][MAX_ERROR_AIRPORT_CHARS];
+/**************************************************************************
+ Function:
+
+ Description: 	Initializes the string of error messages. LOAD_ERRORS is a
+ 	 	 	 					macro defined in the header file.
+
+ Parameters:		None
+
+ Returned:	 		None
+ *************************************************************************/
+void airportLoadErrorMessages ()
+{
+	LOAD_AIRPORT_ERRORS
+}
+/**************************************************************************
+ Function:
+
+ Description:
+ Parameters:		None
+
+ Returned:	 		None
+ *************************************************************************/
+void airportCreate (AirportPtr psTheAirport)
+{
+	queueCreate(&(psTheAirport->sRunwayQueue));
+	pqueueCreate(&(psTheAirport->sInFlightPQueue));
+}
